a93040c11f52fc4f81454b5d3715dc897a3c0660
mahbub-khan <mrkict39@gmail.com>
2024-08-13 22:01:46 +0200

    Logo changed and brought some changes in react helmet

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div>
       <Helmet>
-        <title>StayVista | Vacation Homes & Condo Rentals</title>
+        <title>DreamDwell | Vacation Homes & Condo Rentals</title>
       </Helmet>
       {/* Categories section */}
       <Categories />
